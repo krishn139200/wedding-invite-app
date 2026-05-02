e5c4260ae8752ff30d0794cf9560fe465d022803
krishn kant vashishtha <65147813+krishn139200@users.noreply.github.com>
2026-05-02 11:21:24 +0530

    Change 4

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ if guest_name:
         can.setFont("Helvetica-Bold", 16) 
         
         # NOTE: Adjust x (left/right) and y (up/down) to match the blank line on your card
-        x_coordinate = 230
-        y_coordinate = 400
+        x_coordinate = 200
+        y_coordinate = 380
         
         can.drawString(x_coordinate, y_coordinate, guest_name)
         can.save()
